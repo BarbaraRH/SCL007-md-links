db1c2fe0ddc25fa38ddd996db3c69b5412b99748
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2019-02-19 09:21:58 -0300

    index.js: se crea código que imprime palabras que contengan http en un archivo

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-module.exports = () => {
+/* module.exports = () => {
   // ...
 };
+ */
+
+
+const readline = require('readline');
+const fs = require('fs');
+
+let settings = {
+  input: fs.createReadStream('README.md')
+};
+
+const myInterface = readline.createInterface(settings); 
+
+const printUrl = (data) => {
+  splittedData = data.split(' ');
+  for (let i = 0; i<splittedData.length; i++) {
+    if (splittedData[i].includes("http") === true){
+      console.log(splittedData[i])
+    }    
+  }    
+};
+
+myInterface.on('line', printUrl);   
+
+
+
