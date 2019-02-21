2c248130fe3ae3714677104200ee0280ecf22155
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2019-02-21 17:30:22 -0300

    prueba gitignore

--- index.js
+++ index.js
@@ -3,8 +3,10 @@
 };
  */
 
+// imprime links y valida url, pero da falsos positivos y da desordenado. no imprime linea
 const readline = require('readline');
 const fs = require('fs');
+const urlExists = require('url-exists');
 
 let settings = {
   input: fs.createReadStream('README.md')
@@ -12,12 +14,74 @@ let settings = {
 
 const myInterface = readline.createInterface(settings); 
 
+let lineNumber = 0
 const printUrl = (line) => {
-  let linkMd = line.match(/\[.+\]\(.+\)/g);  
-  if (line.includes(linkMd)){
-    console.log(linkMd[0]);  
-  }     
+  lineNumber++;
+  let regExp = /\[.+\]\((.+)\)/g; //let regExp = /\[.+\]\((.+)\)/g;
+  let match = regExp.exec(line);
+  if (match != null){ 
+    urlExists(match[1], function(err, exists) { 
+    console.log(lineNumber+" "+match[1]  +" "+exists ); 
+     });   
+  }      
 };
 
-myInterface.on('line', printUrl);   
+myInterface.on('line', printUrl);  
+
+
+
+// const readline = require('readline');
+// const fs = require('fs');
+// const urlExists = require('url-exists');
+
+// let settings = {
+//   input: fs.createReadStream('README.md')
+// };
+
+// const myInterface = readline.createInterface(settings); 
+
+
+// let lineNumber = 0
+// const printUrl = (line) => {
+//   lineNumber++;  
+//   let regExp = /\[.+\]\((.+)\)/g; //let regExp = /\[.+\]\((.+)\)/g;
+//   let match = regExp.exec(line);
+//   if (match != null){ 
+//     /* process.stdout.write(lineNumber.toString()+ " " + match[1]+"\n"); */
+//     urlExists(match[1], function(err, exists) {
+//     process.stdout.write(lineNumber.toString()+ " " + match[1]+ " " + exists + "\n");
+//     /* console.log(lineNumber+" "+match[1] +" "+exists); */ 
+//     }); 
+//   }      
+// };
+
+// myInterface.on('line', printUrl);  
+
+/* var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xhr = new XMLHttpRequest();
+
+function doesFileExist(urlToFile) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('HEAD', urlToFile, false);
+  xhr.send();
+   
+  if (xhr.status == "404") {
+      console.log("false");
+  } else {
+    console.log("true");
+  }
+}
+
+doesFileExist("www.googlefalsofalso.com"); */
+
+ 
+
+
+
+
+
+ 
+
+
+ 
 
