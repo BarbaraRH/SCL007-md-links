a86ab2a42a9d4b69a029790129f83191149fdb14
BarbaraRH <barbararamos@MacBook-Air-de-Barbara.local>
2019-02-28 09:15:18 -0300

    se agrega nombre del link

--- index.js
+++ index.js
@@ -7,26 +7,9 @@
  */
 
 
-
 const fs = require('fs');
 const fetch = require('node-fetch')
 const forExtension = require('path');
-
-const fileOrDirectory = (path) => {
-  if (fs.lstatSync(path).isDirectory() === true){
-    /* console.log("soy una carpeta"); */
-    fs.readdirSync(path).forEach(file => {
-      /* console.log(file); */ 
-      if (fs.lstatSync(path + "/" + file).isDirectory() === true || forExtension.extname(path + "/" + file) === ".md"){
-        fileOrDirectory(path + "/" + file); 
-      } /* else {
-        console.log("soy un archivo cualquiera")
-      }  */   
-    }); 
-  } else if (fs.lstatSync(path).isFile() === true && forExtension.extname(path) === ".md"){
-    extractLink(path)
-  }
-}
 
 const validateUrl = (url) => {
   return new Promise((resolve) => {
@@ -44,22 +27,71 @@ const validateUrl = (url) => {
   })
 }
 
+ 
+
+const fileOrDirectory = (path) => {
+  if (fs.lstatSync(path).isDirectory() === true){
+    /* console.log("soy una carpeta"); */
+    fs.readdirSync(path).forEach(file => {
+      /* console.log(file); */ 
+      if (fs.lstatSync(path + "/" + file).isDirectory() === true || forExtension.extname(path + "/" + file) === ".md"){
+        fileOrDirectory(path + "/" + file); 
+      } /* else {
+        console.log("soy un archivo cualquiera")
+      }  */   
+    }); 
+  } else if (fs.lstatSync(path).isFile() === true && forExtension.extname(path) === ".md"){
+    extractLink(path)
+  }
+}
+
+
 const extractLink = (path) => {
-  let links = fs.readFileSync(path).toString().match(/\[.+\]\((.+)\)/gm);
+  let links = fs.readFileSync(path).toString().match(/\[.+\]\(.+\)/gm);
   let urls = []
   for (let i = 0; i < links.length; i++){
     let regExp = /\((.+)\)/g; 
+    let regExpName = /\[(.+)\]/g;
     let match = regExp.exec(links[i]);
-  urls.push({"path":path, "link":match[1]});
+    let linkName = regExpName.exec(links[i]);
+  urls.push({"path":path, "name": linkName[1], "link":match[1]}); 
   }
+
 
   const promiseArr = []
   for (let i = 0; i < urls.length; i++) {
-    promiseArr.push(validateUrl({"path":urls[i].path, "link":urls[i].link}))
+    promiseArr.push(validateUrl({"path":urls[i].path, "name": urls[i].name, "link":urls[i].link}))
   }
 
   Promise.all(promiseArr).then(console.log);
-}
+} 
 
-fileOrDirectory("./prueba");  
+//falla fileOrDirectory al poner espacios en expresión regular ??????
+/*const extractLink = (path) => { 
+
+  let links = fs.readFileSync(path).toString().match(/\s\[.+\]\(.+\)\s/gm); 
+  let urls = []
+    for (let i = 0; i < links.length; i++){
+      let regExp = /\((.+)\)/g; 
+      let match = regExp.exec(links[i]);
+    urls.push({"path":path, "link":match[1]});
+    }
+  
+    const promiseArr = []
+    for (let i = 0; i < urls.length; i++) {
+      promiseArr.push(validateUrl({"path":urls[i].path, "link":urls[i].link}))
+    }
+  
+    Promise.all(promiseArr).then(console.log);  
+}*/
+
+
+
+fileOrDirectory("./prueba");    
+
+
+
+
+
+
 
